85a205950b02869b043abec69da30b44bc555e60
pausun <pfsun9999@msn.com>
2020-10-23 09:41:50 -0800

    Displayed date on HTML page

--- clock.js
+++ clock.js
@@ -1,5 +1,14 @@
-var day = moment().format("dddd");
-var today = moment().format("MMMM Do YYYY");
+const m = moment();
+
+var day = m.format("dddd");
+var today = m.format("MMMM Do YYYY, h:mm:ss a");
 
 console.log(day);
 console.log(today);
+console.log(m.format("[Today is] dddd L"));
+
+
+// Display today's date on html page
+var intervalSeconds = setInterval(function() {
+    $("#date").html(day + " " + today + " " + moment().format("hh:mm:ssa"));
+});
